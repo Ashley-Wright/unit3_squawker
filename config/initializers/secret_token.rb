6bc7c4837ceca7fc2bf8b9748d6443323ef14365
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-02-11 13:19:39 -0600

    Installed rails and postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unit3Squawker::Application.config.secret_key_base = 'f6aa0000cd00c21523eabbc348be4b21780287919c6bcd8c79d17ebedce62210ad081534408bf3fd8ccb500c71eb2fdef159df4fb1877c8d29f00d6588c6cbf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unit3Squawker::Application.config.session_store :cookie_store, key: '_unit3_squawker_session'
